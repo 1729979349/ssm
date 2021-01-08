c8611708ac5e8aa5fe51f99800d80066c3306e89
yzw123456879 <3523557307@qq.com>
2021-01-08 21:03:39 +0800

    yzw

---
+++ src/main/java/com/zking/ssm/entity/User.java
@@ -0,0 +1,14 @@
+package com.zking.ssm.entity;
+
+public class User {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private Integer id;
+
+}
